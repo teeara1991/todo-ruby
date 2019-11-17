6ad620ff6ab76f38bf3686449ead01a0ab788914
teeara1991 <teeara1991@yandex.ru>
2019-11-17 16:55:35 +0300

    added error processing

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,55 +4,58 @@ class Task < ApplicationRecord
   def self.list
     @tasks_uncomplited = Task.where(completed: 0).order(:created_at)
     @tasks_complited = Task.where(completed: 1)
-    puts 'Uncomplited tasks'
     @tasks_uncomplited .each do |task|
       puts "id: #{task.id}
-            title: #{task.title}
-            text: #{task.text}
-            completed: #{task.completed}
-            data: #{task.created_at}"
+              text: #{task.text}
+              completed: #{task.completed}
+              data: #{task.created_at}"
     end
-    puts '----------------------------------------------'
-    puts 'Complited tasks'
     @tasks_complited .each do |task|
       puts "id: #{task.id}
-      title: #{task.title}
-      text: #{task.text}
-      completed: #{task.completed}
-      data: #{task.created_at}"
+        text: #{task.text}
+        completed: #{task.completed}
+        data: #{task.created_at}"
     end
+  rescue StandardError
+    puts 'Error showing list of task'
   end
 
   def self.create(options = {})
     @task = Task.new
-    @task.title = options[:title]
-    @task.text = options[:text]
+    @task.text = options[:text].to_s
     @task.completed = false
-    @task.save
+    @task.save if @task.text != ''
+  rescue StandardError
+    puts 'Error creating task, please check input parameters'
   end
 
   def self.change(options = {})
     @task = Task.find(options[:id])
-    @task.title = options[:title]
-    @task.text = options[:text]
-    @task.save
+    @task.text = options[:text].to_s
+    @task.save if @task.text != ''
+  rescue StandardError
+    puts 'Error changing task, please check input parameters'
   end
 
   def self.complete(options = {})
     @task = Task.find(options[:id])
     @task.completed = !@task.completed
     @task.save
+  rescue StandardError
+    puts 'Error changing status of task, please check input parameters'
   end
 
   def self.delete(options = {})
     @task = Task.find(options[:id])
     @task.destroy
+  rescue StandardError
+    puts 'Error deleting task, please check input parameters'
   end
 
   def self.deleteAll
     @tasks = Task.all
-    @tasks .each do |task|
-      task.destroy
-    end
+    @tasks .each(&:destroy)
+  rescue StandardError
+    puts 'Error deleting tasks'
   end
 end
